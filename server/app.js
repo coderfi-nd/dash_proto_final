6bdabd8ce58ef0d6210acca746fa1a04a24ac5a3
PavelEE <zaytsev.pavel.a@gmail.com>
2015-08-12 09:00:24 -0700

    data stuff figured out

--- server/app.js
+++ server/app.js
@@ -17,6 +17,29 @@ var routes = require('./routes/index')
 
 var app = express();
 
+
+function csvToJSON(csv){
+	var lines = csv.split("\n");
+	var result = [];
+	var headers = lines[0].split(",");
+	for (var i = 1; i < lines.length; i++)
+	{
+		var obj = {};
+		var currentline = lines[i].split(",");
+
+		for (var j=0; j < headers.length; j++)
+		{
+			obj[headers[j]] = currentline[j];
+		}
+
+		result.push(obj);
+
+	}
+
+	return JSON.stringify(result);
+
+
+}
 // parser and template middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,16 +65,31 @@ app.use(function(req, res, next) {
 			fs.readFile(path.join(__dirname, dir) + file, 'utf-8', function(err, data)
 			{
 				if(err) throw err;
-				sendable[file] = data;
+				sendable[file.split('.')[0]] = csvToJSON(data);
 				if(0 ===-- c){
 
-					console.log(sendable);
+					// store data in the variable and go to next middleware
+					req.sendable = sendable;
+					next();
 				}
 
 			});
 		});
 	});
-	next();
+	
+});
+
+// save obtained files to lci collection
+app.use(function(req, res, next) {
+
+	var db = req.db;
+	var sendable = req.sendable;
+	var lci_collection = db.get('lci');
+	lci_collection.insert(sendable, function(err, doc) {
+		if(err) throw err;
+		next();
+	});
+
 });
 
 // all routes
